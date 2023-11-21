2566533a27b7942b491bc9dce3a65e9ae1d626df
joaoguilherme2003 <joao.mendonca@encora.com>
2023-11-21 11:10:54 -0300

    Add files via upload

---
+++ se-front/firebase.js
@@ -0,0 +1,15 @@
+(function () {
+    var firebaseConfig = {
+        apiKey: "AIzaSyBSdMDnbDbHA5YQ0-qt3l_c-WWOVfm22z8",
+        authDomain: "se-cesar-a3244.firebaseapp.com",
+        databaseURL: "https://se-cesar-a3244-default-rtdb.firebaseio.com",
+        projectId: "se-cesar-a3244",
+        storageBucket: "se-cesar-a3244.appspot.com",
+        messagingSenderId: "811140211073",
+        appId: "1:811140211073:web:2681cb914050d8a9a7c880",
+        measurementId: "G-NV7EVJ68XV"
+      };
+    
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+})()
